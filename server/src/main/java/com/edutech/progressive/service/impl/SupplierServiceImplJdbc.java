2bd926c8c9ebd4eac2370b377b4114a1419ba665
Piyush Gajbhiye <159016704+piyushgajbhiye05@users.noreply.github.com>
2026-02-02 19:09:10 +0530

    Refactor SupplierServiceImpl to use JDBC

--- server/src/main/java/com/edutech/progressive/service/impl/SupplierServiceImplJdbc.java
+++ server/src/main/java/com/edutech/progressive/service/impl/SupplierServiceImplJdbc.java
@@ -1,40 +1,58 @@
 package com.edutech.progressive.service.impl;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 
 import java.util.Collections;
 import java.util.List;
 
+import com.edutech.progressive.dao.SupplierDAO;
+
 import com.edutech.progressive.entity.Supplier;
 import com.edutech.progressive.service.SupplierService;
 
-public class SupplierServiceImplArraylist implements SupplierService {
-    List<Supplier> suplist = new ArrayList<>();
+public class SupplierServiceImplJdbc implements SupplierService {
+
+
+     private SupplierDAO supplierDAO;
+
+     
+    public SupplierServiceImplJdbc(SupplierDAO supplierDAO) {
+        this.supplierDAO = supplierDAO;
+    }
+
+    
     @Override
-    public List<Supplier> getAllSuppliers() {
-        // TODO Auto-generated method stub
-       // throw new UnsupportedOperationException("Unimplemented method 'getAllSuppliers'");
-       return suplist;
+    public List<Supplier> getAllSuppliers() throws SQLException{
+       
+       
+       return supplierDAO.getAllSuppliers();
     }
 
     @Override
-    public int addSupplier(Supplier supplier) {
-        // TODO Auto-generated method stub
-       // throw new UnsupportedOperationException("Unimplemented method 'addSupplier'");
-       suplist.add(supplier); 
-       return suplist.size();
+    public int addSupplier(Supplier supplier) throws SQLException{
+       
+       return supplierDAO.addSupplier(supplier);
     }
 
     @Override
-    public List<Supplier> getAllSuppliersSortedByName() {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'getAllSuppliersSortedByName'");
-        Collections.sort(suplist);
-        return suplist;
+    public List<Supplier> getAllSuppliersSortedByName() throws SQLException{
+     
+        List<Supplier> list = supplierDAO.getAllSuppliers();
+        Collections.sort(list);
+        return list;
+      
     
     }
-    //public void emptyArray(){
-     //   
-    //}
-
+    @Override
+    public void updateSupplier(Supplier supplier) throws SQLException {
+        supplierDAO.updateSupplier(supplier);
+    }
+    @Override
+    public void deleteSupplier(int supplierId) throws SQLException {
+        supplierDAO.deleteSupplier(supplierId);
+    }
+    @Override
+    public Supplier getSupplierById(int supplierId) throws SQLException{
+        return supplierDAO.getSupplierById(supplierId);
+    }
 }
