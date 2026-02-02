1a58d176cc8ad9ed78f9748353c6684832c14777
Piyush Gajbhiye <159016704+piyushgajbhiye05@users.noreply.github.com>
2026-02-02 18:08:55 +0530

    Update ProductDAOImpl.java

--- server/src/main/java/com/edutech/progressive/dao/ProductDAOImpl.java
+++ server/src/main/java/com/edutech/progressive/dao/ProductDAOImpl.java
@@ -11,89 +11,85 @@ import java.util.List;
 import com.edutech.progressive.config.DatabaseConnectionManager;
 import com.edutech.progressive.entity.Product;
 
-public class ProductDAOImpl implements ProductDAO {
-    private Connection connection;
-    public ProductDAOImpl() {
-        this.connection = DatabaseConnectionManager.getConnection();
-    }
+public class ProductDAOImpl implements ProductDAO{
+ public ProductDAOImpl() {}
 
-    @Override
     public int addProduct(Product product) throws SQLException {
-        String sql = "INSERT INTO product(warehouse_id,product_name,product_description,quantity,price) Values(?,?,?,?,?)";
-        PreparedStatement ps = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+        String sql = "INSERT INTO product (warehouse_id, product_name, product_description, quantity, price) VALUES (?,?,?,?,?)";
+        Connection con = DatabaseConnectionManager.getConnection();
+        PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
         ps.setInt(1, product.getWarehouseId());
         ps.setString(2, product.getProductName());
         ps.setString(3, product.getProductDescription());
         ps.setInt(4, product.getQuantity());
-        ps.setDouble(5, product.getPrice());
+        ps.setLong(5, product.getPrice());
         ps.executeUpdate();
+
         ResultSet rs = ps.getGeneratedKeys();
-        if(rs.next()){
-            int id=rs.getInt(1);
-            product.setProductId(id);
-            return id;
+        if (rs.next()) {
+            product.setProductId(rs.getInt(1));
+            return product.getProductId();
         }
         return -1;
-  
     }
 
-    @Override
-    public Product getProductById(int productId)throws SQLException {
-        Product product = null;
-        String sql = "SELECT * FROM product WHERE product_id=?";
-        PreparedStatement ps = connection.prepareStatement(sql);
+    public Product getProductById(int productId) throws SQLException {
+        Connection con = DatabaseConnectionManager.getConnection();
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM product WHERE product_id=?");
         ps.setInt(1, productId);
         ResultSet rs = ps.executeQuery();
-        if(rs.next()){
-            int id = rs.getInt("product_id");
-            int warehouse_id = rs.getInt("warehouse_id");
-            String product_name = rs.getString("product_name");
-            String product_description = rs.getString("product_description");
-            int quantity = rs.getInt("quantity");
-            Long price = rs.getLong("price");
-            return product = new Product(id, warehouse_id, product_name, product_description, quantity, price);
+
+        if (rs.next()) {
+            Product p = new Product();
+            p.setProductId(rs.getInt("product_id"));
+            p.setWarehouseId(rs.getInt("warehouse_id"));
+            p.setProductName(rs.getString("product_name"));
+            p.setProductDescription(rs.getString("product_description"));
+            p.setQuantity(rs.getInt("quantity"));
+            p.setPrice(rs.getLong("price"));
+            return p;
         }
         return null;
     }
 
-    @Override
     public void updateProduct(Product product) throws SQLException {
-        String sql ="UPDATE product SET product_name=?,product_description=?,quantity=?,price=? WHERE product_id=?";
-        PreparedStatement ps = connection.prepareStatement(sql);
+        Connection con = DatabaseConnectionManager.getConnection();
+        PreparedStatement ps = con.prepareStatement(
+                "UPDATE product SET product_name=?, product_description=?, quantity=?, price=? WHERE product_id=?");
+
         ps.setString(1, product.getProductName());
         ps.setString(2, product.getProductDescription());
         ps.setInt(3, product.getQuantity());
         ps.setLong(4, product.getPrice());
-        ps.setInt(1, product.getProductId());
+        ps.setInt(5, product.getProductId());
         ps.executeUpdate();
     }
-    @Override
+
     public void deleteProduct(int productId) throws SQLException {
-        String sql ="DELETE FROM product WHERE product_id=?";
-        PreparedStatement ps = connection.prepareStatement(sql);
+        Connection con = DatabaseConnectionManager.getConnection();
+        PreparedStatement ps = con.prepareStatement("DELETE FROM product WHERE product_id=?");
         ps.setInt(1, productId);
         ps.executeUpdate();
     }
 
-    @Override
     public List<Product> getAllProducts() throws SQLException {
-        List<Product> plist = new ArrayList<>();
-        String sql = "SELECT * FROM product";
-        PreparedStatement ps = connection.prepareStatement(sql);
+        List<Product> list = new ArrayList<>();
+        Connection con = DatabaseConnectionManager.getConnection();
+        PreparedStatement ps = con.prepareStatement("SELECT * FROM product");
         ResultSet rs = ps.executeQuery();
-        while(rs.next()){
-            int id = rs.getInt("product_id");
-            int warehouse_id = rs.getInt("warehouse_id");
-            String product_name = rs.getString("product_name");
-            String product_description = rs.getString("product_description");
-            int quantity = rs.getInt("quantity");
-            Long price = rs.getLong("price");
-            Product product = new Product(id, warehouse_id, product_name, product_description, quantity, price);
-            plist.add(product);
-            
+
+        while (rs.next()) {
+            Product p = new Product();
+            p.setProductId(rs.getInt("product_id"));
+            p.setWarehouseId(rs.getInt("warehouse_id"));
+            p.setProductName(rs.getString("product_name"));
+            p.setProductDescription(rs.getString("product_description"));
+            p.setQuantity(rs.getInt("quantity"));
+            p.setPrice(rs.getLong("price"));
+            list.add(p);
         }
-        return plist;
- 
+        return list;
     }
 
 }
